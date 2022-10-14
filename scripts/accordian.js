d38698335221cb1192fc230e5717c7b7549908f0
Peter Rydden <peter.ryden@utb.ecutbildning.se>
2022-10-14 23:31:36 +0200

    task solved

--- scripts/accordian.js
+++ scripts/accordian.js
@@ -1,2 +1,17 @@
 
 // you should use a loop ...
+"use script";
+let question = document.getElementsByClassName('accordian-container');
+
+for(let i =0; i < question.length; i++){
+  question[i].addEventListener('click', function(){
+    this.classList.toggle("active")
+  });
+}
+/*let divElement = document.createElement('div');
+divElement.classList.add('divmenu');
+console.log(divElement);
+document.body.append(divElement);
+let navElement = '<nav><ul><li><a href="https://www.w3schools.com/html/default.asp">HTML</a> </li><li><a href="https://www.w3schools.com/css/default.asp">CSS</a> </li><li><a href="https://www.w3schools.com/js/default.asp">JavaScript</a> </li><li><a href="https://www.w3schools.com/python/default.asp">Python</a></li></ul></nav>';
+divElement.insertAdjacentHTML('afterbegin', navElement);*/
+ 
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,1 +1,31 @@
+let header = document.getElementsByTagName('header');
+header[0].style.backgroundColor = "rgb(87, 87, 87)";
+header[0].style.color = "white";
 
+window.onload = () => {
+let divElement = document.createElement('div');
+divElement.classList.add('divElement','accordian');
+let navElement = '<nav><ul><li><a href="https://www.w3schools.com/html/default.asp">HTML</a> </li><li><a href="https://www.w3schools.com/css/default.asp">CSS</a> </li><li><a href="https://www.w3schools.com/js/default.asp">JavaScript</a> </li><li><a href="https://www.w3schools.com/python/default.asp">Python</a></li></ul></nav>';
+divElement.insertAdjacentHTML('afterBegin', navElement);
+console.log(navElement);
+let divplace = document.querySelector('.flex-container');
+console.log(divplace);
+divplace.parentNode.insertBefore(divElement, divplace);
+divElement.style.width = "90%"
+divElement.style.marginBottom = "20px"
+divElement.style.backgroundColor = "rgb(210, 210, 190)";
+let ul = document.getElementsByTagName('ul');
+ul[0].style.display = "flex";
+ul[0].style.listStyleType = "none";
+let li = document.getElementsByTagName('li');
+for(let i =0; i < li.length; i++){
+    li[i].style.textAlign = "center";
+}
+for(let i =0; i < li.length; i++){
+    li[i].style.paddingLeft = "10%";
+}
+let a = document.getElementsByTagName('a');
+for(let i =0; i < a.length; i++){
+    a[i].style.textDecoration = "none";
+}
+}
