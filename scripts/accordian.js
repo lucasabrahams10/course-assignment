181eba1a79bab30adb67a0ccf47802a3329246c3
andresparrab <andres.parrab@gmail.com>
2022-10-17 22:41:44 +0200

    It is done

--- scripts/accordian.js
+++ scripts/accordian.js
@@ -1,2 +1,13 @@
 
 // you should use a loop ...
+console.log("Starting")
+
+const accordian = document.getElementsByClassName('accordian-container')
+// const answer =document.getElementsByClassName("answer")
+console.log(accordian)
+
+for(let i=0; i < accordian.length; i++){
+    accordian[i].addEventListener("click", function() {
+        this.classList.toggle("active");        
+    })
+}
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,1 +1,34 @@
+window.onload = () => {
+    const header = document.getElementsByTagName('header')
+    var navigation = `
+        <nav>
+        <a href="../index.html">Home</a>
+        <a href="products.html">Products</a>
+        <a href="about.html">About us</a>
+        <a href="contact.html">Contact us</a>
+        </nav>
+        `
 
+    header[0].style.background = "rgb(225,233,232)";
+
+    //inserting the nav bar
+    header[0].insertAdjacentHTML('beforeend', navigation)
+
+    //Adding some style to the nav
+    var nav = document.getElementsByTagName('nav')
+    nav[0].style.background = "rgb(225,233,232)";
+    nav[0].style.padding = "1vw";
+
+    //get the links and add some style
+    const links = document.getElementsByTagName("a")
+
+    for (let index = 0; index < links.length; index++) {
+        links[index].style.marginRight = "4vw"
+        links[index].style.textDecoration = "none"
+        links[index].style.borderRadius = "6px"
+        links[index].style.padding = "0.5vw";
+        links[index].style.boxShadow = `0px 6px 13px -7px #000000, 5px -50px 15px 5px rgba(0,0,0,0)`
+
+    }
+
+}
